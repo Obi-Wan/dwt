deee3fa9840611d919847fb50799bc5868af15e9
Nicola Viganò <ben.vighy@gmail.com>
2013-07-12 23:41:19 +0200

    Matlab interface: Added initial stub
    Signed-off-by: Nicola Viganò <ben.vighy@gmail.com>

---
+++ matlab/dwt_haar.cpp
@@ -0,0 +1,15 @@
+/*
+ * dwt_haar.cpp
+ *
+ *  Created on: 12/lug/2013
+ *      Author: ben
+ */
+
+#include "mex.h"
+
+#include "dwt_cpp.h"
+
+void mexFunction(int nlhs, mxArray * plhs[], int nrhs, const mxArray * prhs[])
+{
+}
+
